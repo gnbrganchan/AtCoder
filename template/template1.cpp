967868ebbb8038b6bdb61cade0f29e06742bc138
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-08-11 22:00:11 +0900

    Add files via upload

--- template/template1.cpp
+++ template/template1.cpp
@@ -1,13 +1,18 @@
 #include<iostream>
 #include <algorithm>
 #include <functional>
+#include<vector>
+#include<unordered_map>
 
 using namespace std;
 
 int main(){
 
 string s;
-int a,b,c,ans;
+int a,b,c,i,j,k,n,ans;
+vector<string> lst;
+vector<int> cnt;
+unordered_map<string,int> mp;
 
 cin >> a >> b;
 
