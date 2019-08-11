cab2a33b32d7f90fe12b8f091839f604c768e735
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-08-11 21:58:21 +0900

    Add files via upload

---
+++ template/template1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+
+string s;
+int a,b,c,ans;
+
+cin >> a >> b;
+
+
+
+cout << ans << endl;
+
+return 0;
+
+}
