5cbe204572f8bc4780eee51a6fb27e9a785aee0b
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-08-11 17:09:08 +0900

    Add files via upload

---
+++ template/一項目ずつ読み込むテンプレート.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+int main(){
+
+int a,b,c,ans;
+
+cin >> a >> b;
+
+
+
+cout << ans << endl;
+
+return 0;
+
+}
