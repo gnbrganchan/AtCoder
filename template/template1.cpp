3b4cc3c4cda8f213a89821e81e2a36609578a76c
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-08-12 22:02:08 +0900

    Add files via upload

--- template/template1.cpp
+++ template/template1.cpp
@@ -3,16 +3,29 @@
 #include <functional>
 #include<vector>
 #include<unordered_map>
+#include<queue>
 
 using namespace std;
+class CompareFirst
+{
+public:
+    bool operator()(pair<int,int> n1,pair<int,int> n2) {
+        return n1.first>n2.first; //>:¸‡@<:~‡
+    }
+};
+
 
 int main(){
 
 string s;
-int a,b,c,i,j,k,n,m,ans;
+int a,b,c,i,j,k,n,ans;
 vector<string> lst;
 vector<int> cnt;
-unordered_map<string,int> mp;
+
+//unordered_map<string,int> mp;
+
+//priority_queue<int> pq; // default ‘å‚«‚¢‡
+//priority_queue<pair<int,int>,vector<pair<int,int>>,CompareFirst pq;
 
 cin >> a >> b;
 
