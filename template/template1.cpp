1182c7e97f157bae8aaf5f2d987f3ddf1664fb48
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-08-27 11:07:27 +0900

    Update template1.cpp

--- template/template1.cpp
+++ template/template1.cpp
@@ -5,7 +5,8 @@
 #include <vector>
 #include <unordered_map>
 #include <queue>
-#define rep(i,n) for(int i=1;i<=n;++i)
+#define rep0(i,n) for(int i=0;i<n;++i)
+#define rep1(i,n) for(int i=1;i<=n;++i)
 
 using namespace std;
 class CompareFirst
