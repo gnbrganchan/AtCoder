2aae6e35f736c0baf45d8a901a93c61be103e563
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-09-12 23:17:18 +0900

    Update template1.cpp

--- template/template1.cpp
+++ template/template1.cpp
@@ -2,6 +2,7 @@
 #define rep(i,cc,n) for(int i=cc;i<=n;++i)
 #define drep(i,cc,n) for(int i=cc;i>=n;--i)
 #define sz(s) (int)(s.size())
+#define vecprint(v) rep(i,0,v.size()-1)cout << v[i] << " ";cout << endl;
 #define mod 1000000007
 
 using namespace std;
