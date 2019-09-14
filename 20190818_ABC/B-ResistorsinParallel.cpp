f379f452f0e501529714a401588751c33f17e6ba
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-09-14 23:04:23 +0900

    Update B-ResistorsinParallel.cpp

--- 20190818_ABC/B-ResistorsinParallel.cpp
+++ 20190818_ABC/B-ResistorsinParallel.cpp
@@ -3,15 +3,15 @@ using namespace std;
  
 int main(){ 
   int i,n;
-  double ans=0.0;
+  double ans=0.0;//double型で宣言、初期化
   
   cin >> n;
   int a[n];
-  for(i=0;i<n;++i)cin >> a[i];
+  for(i=0;i<n;++i)cin >> a[i];//入力を配列に格納
 
-  for(i=0;i<n;i++)ans += (double)1/a[i];
-  ans = 1.0/ans;
+  for(i=0;i<n;i++)ans += (double)1/a[i];//逆数を足していく
+  ans = 1.0/ans;//最後に逆数をとる
 
-  printf("%.10f\n",ans);
+  printf("%.10f\n",ans);//小数点以下10桁まで出力
   return 0;
 }
