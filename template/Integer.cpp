cbcd13da1b374530ec2f0ae88986c0fe324db408
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-09-30 00:20:58 +0900

    Update Integer.cpp

--- template/Integer.cpp
+++ template/Integer.cpp
@@ -1,7 +1,10 @@
+//最大公約数 from 20190929
+long long gcd(long long x, long long y) { return y ? gcd(y,x%y) : x;}
+
 //素因数分解 from 20190929
-vector<pair<ll,int>> factorize(ll n) {
-  vector<pair<ll,int>> res;
-  for (ll i = 2; i*i <= n; ++i) {
+vector<pair<long long,int>> factorize(long long n) {
+  vector<pair<long long,int>> res;
+  for (long long i = 2; i*i <= n; ++i) {
     if (n%i) continue;
     res.emplace_back(i,0);
     while (n%i == 0) {
