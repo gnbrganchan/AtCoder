9ebe6f471cc81fc11cfb511915b6282124d73eb3
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-09-30 00:17:42 +0900

    Update trees.cpp

--- template/trees.cpp
+++ template/trees.cpp
@@ -1,3 +1,4 @@
+//深さ優先探索
 void dfs(int v, int p){
   for(auto u : vec[v]){
     if(u == p)continue;
@@ -5,3 +6,36 @@ void dfs(int v, int p){
     dfs(u,v);
   }
 }
+
+//幅優先探索 from 20190929
+vector<int> bfs(int sv) {
+  vector<int> dist(n,INF), pre(n,-1);
+  queue<int> q;
+  dist[sv] = 0;
+  q.push(sv);
+  while (!q.empty()) {
+    int v = q.front(); q.pop();
+    for (int u : to[v]) {
+      if (dist[u] != INF) continue;
+      pre[u] = v;
+      dist[u] = dist[v]+1;
+      q.push(u);
+    }
+  }
+ 
+  pair<int,int> best(INF,-1);
+  rep(v,n) {
+    if (v == sv) continue;
+    for (int u : to[v]) {
+      if (u == sv) {
+        best = min(best, make_pair(dist[v],v));
+      }
+    }
+  }
+  if (best.first == INF) return vector<int>(n+1);
+  int v = best.second;
+  vector<int> res;
+  while (v != -1) {
+    res.push_back(v);
+    v = pre[v];
+  }
