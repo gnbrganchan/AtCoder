b8aa45c605d28ad479169280d75c0869db99c643
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-12-17 22:00:36 +0900

    Update Integer.cpp

--- template/Integer.cpp
+++ template/Integer.cpp
@@ -1,3 +1,14 @@
+//fast pow
+ll mod_pow(ll x,ll n){
+  ll res = 1;
+  while(n > 0){
+    if(n & 1) res = res * x % mod;
+    x = x * x % mod;
+    n >>= 1;
+  }
+  return res;
+}
+
 //mint
 struct mint {
   ll x;
