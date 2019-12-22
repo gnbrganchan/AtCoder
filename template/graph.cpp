e8412d3637e53f1eb676f968c854fcfb73c93090
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-12-22 13:14:31 +0900

    Update and rename trees.cpp to graph.cpp

--- template/graph.cpp
+++ template/graph.cpp
@@ -1,7 +1,5 @@
 struct edge( int to,cost;}; //P92
 
-
-
 //深さ優先探索 P33
 void dfs(int v, int p){
   for(auto u : vec[v]){
@@ -43,6 +41,7 @@ vector<int> bfs(int sv) {
     res.push_back(v);
     v = pre[v];
   }
+}
   
 //Union-Find木 P84
 int par[MAX_N];  //親
@@ -79,4 +78,45 @@ void unite(int x, int y){
 bool same(int x, int y){
   return find(x) == find(y);
 }
-    
+
+//最大流 Ford Fulkerson P190
+//辺を表す構造体（行先、容量、逆辺）
+struct edge {int to,cap, rev;};
+
+vector<edge> G[MAX_V];//グラフの隣接リスト表現
+bool used[MSX_V];//dfsですでに調べたかのフラグ
+
+//from からtoへ向かう容量capの辺をグラフに追加する
+void add_edge(int from, int to, int cap){
+  G[from].push_back((edge){to, cap, G[to].size()});
+  G[to].push_back((edge){from, 0, G[from].size()-1});
+}
+  
+//増加パスをdfsで探す
+int dfs(int v, int t, int f){
+  if(v == t) return f;
+  used[v] = true;
+  rep(i,0,G[v].size()-1){
+    edge &e = G[v][i];
+    if(!used[e.to] && e.cap > 0){
+      int d = dfs(e.to,t,min(f, e.cap));
+      if(d > 0){
+        e.cap -= d;
+        G[e.to][e.rev].cap += d;
+        return d;
+      }
+    }
+  }
+  return 0;
+}
+//sからtへの最大流を求める
+int max_flow(int s, int t){
+  int flow = 0;
+  while(true){
+    memset(used, 0, sizeof(used));
+    int f = dfs(s,t,INF);
+    if(f == 0) return flow;
+    flow += f;
+  }
+}
+      
