3edb3a69e85c5952a9df894110ef5d3d3dd1fdea
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2019-12-23 23:22:53 +0900

    Update graph.cpp

--- template/graph.cpp
+++ template/graph.cpp
@@ -1,4 +1,4 @@
-struct edge( int to,cost;}; //P92
+struct edge{ int to,cost;}; //P92
 
 //深さ優先探索 P33
 void dfs(int v, int p){
@@ -8,6 +8,7 @@ void dfs(int v, int p){
     dfs(u,v);
   }
 }
+
 
 //幅優先探索 from 20190929 P36
 vector<int> bfs(int sv) {
@@ -43,6 +44,7 @@ vector<int> bfs(int sv) {
   }
 }
   
+
 //Union-Find木 P84
 int par[MAX_N];  //親
 int rank[MAX_N] //木の深さ
@@ -79,6 +81,7 @@ bool same(int x, int y){
   return find(x) == find(y);
 }
 
+
 //最大流 Ford Fulkerson P190
 //辺を表す構造体（行先、容量、逆辺）
 struct edge {int to,cap, rev;};
@@ -104,7 +107,7 @@ int dfs(int v, int t, int f){
         e.cap -= d;
         G[e.to][e.rev].cap += d;
         return d;
-      }
+      }int 
     }
   }
   return 0;
@@ -118,5 +121,66 @@ int max_flow(int s, int t){
     if(f == 0) return flow;
     flow += f;
   }
+}
+
+
+//最小費用流 P200
+//辺を表す構造体（行先、容量、コスト、逆辺）
+struct edge{int to,cap, cost, rev;};
+
+int V; //頂点数
+vector<edge> G[MAX_V]; //グラフの隣接リスト表現
+int dist[MAX_V]; //最短距離
+int prevv[MAX_V], preve[MAX_V]; //直前の頂点と辺
+
+//fromからtoへ向かう容量cap、コストcostの辺をグラフに追加する
+void add_edge(int from, int to, int cap, int cost){
+  G[from].push_back((edge){to, cap, cost, G[to].size()});
+  G[to].push_back((edge){from, 0, -cost, G[from].size()-1});
+}
+
+//sからtへの流量fの最小費用流を求める
+//流せない場合は-1を返す
+int min_cost_flow(int s, int t, int f){
+  int res = 0;
+  while(f > 0){
+    //ベルマンフォード法により、s-t間最短路を求める
+    fill(dist, dist+V,INF);
+    dist[s] = 0;
+    bool update = true;
+    while(update){
+      update = false;
+      rep(v,0,V-1){
+        if(dist[v] == INF) continue;
+        rep(i,0,G[v].size()-1){
+          edge &e = G[v][i];
+          if(e.cap > 0 && dist[e.to] > dist[v] + e.cost){
+            dist[e.to] = dist[v] + e.cost;
+            prevv[e.to] = v;
+            preve[e.to] = i;
+            update = true;
+
+          }
+        }
+      }
+    }
+    if(dist[t] == INF){
+      //これ以上流せない
+      return -1;
+    }
+    //s-t間最短路に沿ってめいっぱい流す
+    int d = f;
+    for(int v=t;v!=s;v=prevv[v]){
+      d = min(d, G[prevv[v]][preve[v]].cap);
+    }
+    f -= d;
+    res += d * dist[t];
+    for(int v=t;v!=s;v=prevv[v]){
+      edge &e = G[prevv[v]][preve[v]];
+      e.cap -= d;
+      G[v][e.rev].cap += d;
+    }
+  }
+  return res;
 }
       
