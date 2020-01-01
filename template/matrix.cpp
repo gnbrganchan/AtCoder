4fb0349b08af033f1130c0ce477fca0e59479a8a
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2020-01-01 23:37:59 +0900

    Update matrix.cpp

--- template/matrix.cpp
+++ template/matrix.cpp
@@ -1,7 +1,8 @@
 typedef vector<int> vec;
 typedef vector<vec> mat;
+const double EPS = 1E-8;
 
-//A*B
+/*---A*B^^^*/
 mat mul(mat &A, mat &B){
   mat C(A.size(),vec(B[0].size()));
   rep(i,0,A.size()-1){
@@ -14,7 +15,8 @@ mat mul(mat &A, mat &B){
   return C;
 }
 
-//A^n
+
+/*---A^n---*/
 mat pow(mat A, ll n){
   mat B(A.size(),vec(A.size()));
   rep(i,0,A.size()-1){
@@ -26,4 +28,39 @@ mat pow(mat A, ll n){
     n >>= 1;
   }
   return B;
+}
+
+
+
+/*---連立一次方程式の解(Gauss-Jordan) P257---*/
+//Ax = bを解く
+//解がないか一意でない場合は長さ0の配列を返す
+vec gauss_jordan(const mat& A, const vec& b){
+  int n = A.size();
+  mat B(n, vec(n+1));
+  rep(i, 0, n-1)rep(j, 0, n-1)B[i][j] = A[i][j];
+  //行列Aの後ろにbを並べて同時に処理する
+  rep(i,0,n-1)B[i][n] = b[i];
+  
+  rep(i,0,n-1){
+    int pivot = i;
+    rep(j,i,n-1)if(abs(B[j][i]) > abs(B[pivot][i]))pivot = j;
+    swap(B[i],B[pivot]);
+    
+    //解がないか一意でない
+    if(abs(B[i][i] < EPS)) return vec();
+    
+    //注目している変数の係数を1にする
+    rep(j,i+1,n)B[i][j] /= B[i][i];
+    rep(j,0,n-1){
+      if(i != j){
+        //j番目の式からi番目の変数を消去
+        rep(k,i+1,n)B[j][k] -= B[j][i] * B[i][k];
+      }
+    }
+  }
+  vec x(n);
+  //後ろに並べたbが解になる
+  rep(i,0,n-1)x[i] = B[i][n];
+  return x;
 }
