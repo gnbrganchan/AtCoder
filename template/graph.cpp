efcb8ac298e3635e26738424462f8de969171f26
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2020-03-01 22:42:25 +0900

    Update graph.cpp

--- template/graph.cpp
+++ template/graph.cpp
@@ -182,41 +182,52 @@ int kruskal(){
 
 
 /*---Union-Find木 P84---*/
-int par[MAX_N];  //親
-int rank[MAX_N] //木の深さ
-//初期化
-void init(int n){
-  rep(i,0,n-1){
-    par[i] = i;
-    rank[i] = 0;
-  }
-}
-//木の根を求める
-int find(int x){
-  if(par[x] == x){
-    return x;
-  }else{
-    return par[x] = find(par[x]);
-  }
-}
-//xとyの属する集合を併合
-void unite(int x, int y){
-  x = find(x);
-  y = find(y);
-  if(x == y) return;
-  
-  if(rank[x] < rank[y]){
-    par[x] = y;
-  }else{
-    par[y] = x;
-    if(rank[x] == rank[y]) rank[x]++;
-  }
-}
-//xとyが同じ集合に属するか否か
-bool same(int x, int y){
-  return find(x) == find(y);
-}
+https://pyteyon.hatenablog.com/entry/2019/03/11/200000
+// union by size + path having
+class UnionFind {
+public:
+    vector <ll> par; // 各元の親を表す配列
+    vector <ll> siz; // 素集合のサイズを表す配列(1 で初期化)
 
+    // Constructor
+    UnionFind(ll sz_): par(sz_), siz(sz_, 1LL) {
+        for (ll i = 0; i < sz_; ++i) par[i] = i; // 初期では親は自分自身
+    }
+    void init(ll sz_) {
+        par.resize(sz_);
+        siz.assign(sz_, 1LL);  // resize だとなぜか初期化されなかった
+        for (ll i = 0; i < sz_; ++i) par[i] = i; // 初期では親は自分自身
+    }
+
+    // Member Function
+    // Find
+    ll root(ll x) { // 根の検索
+        while (par[x] != x) {
+            x = par[x] = par[par[x]]; // x の親の親を x の親とする
+        }
+        return x;
+    }
+
+    // Union(Unite, Merge)
+    bool merge(ll x, ll y) {
+        x = root(x);
+        y = root(y);
+        if (x == y) return false;
+        // merge technique（データ構造をマージするテク．小を大にくっつける）
+        if (siz[x] < siz[y]) swap(x, y);
+        siz[x] += siz[y];
+        par[y] = x;
+        return true;
+    }
+
+    bool issame(ll x, ll y) { // 連結判定
+        return root(x) == root(y);
+    }
+
+    ll size(ll x) { // 素集合のサイズ
+        return siz[root(x)];
+    }
+};
 
 
 /*---最大流 Ford Fulkerson P190---*/
