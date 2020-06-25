8e9ad7d8fffa57e5a7400f517f70675929216c4b
gnbrganchan <53957532+gnbrganchan@users.noreply.github.com>
2020-06-25 22:39:30 +0900

    Update template.cpp

--- template/template.cpp
+++ template/template.cpp
@@ -1,18 +1,27 @@
 #include <bits/stdc++.h>
-#define rep(i,cc,n) for(int i=cc;i<=n;++i)
-#define drep(i,cc,n) for(int i=cc;i>=n;--i)
+#define drep(i,cc,n) for(int i=(cc);i<=(n);++i)
+#define rep(i,n) drep(i,0,n-1)
+#define drrep(i,cc,n) for(int i=(cc);i>=(n);--i)
+#define rrep(i,n) drrep(i,0,n-1)
 #define sz(s) (int)(s.size())
-#define vecprint(v) rep(i,0,v.size()-1)cout << v[i] << " ";cout << endl;
-#define mod 1000000007
-typedef long long ll;
-typedef pair<int,int> P;
+#define vecprint(v) rep(i,v.size())if(i==0)cout << v[i];else cout << " " << v[i]; cout << endl;
+#define arrprint(v,n) rep(i,n)if(i==0)cout << v[i];else cout << " " << v[i]; cout << endl;
+#define matprint(v,n,m) rep(i,n){rep(j,m){cout << v[i][j] << " ";}cout << endl;}
+#define SORT(v) sort(v.begin(), v.end())
+#define REV(v) reverse(v.begin(), v.end())
 using namespace std;
+const int mod=1000000007;
+const int INF=1001001001;
+using ll = long long;
+using P = pair<int,int>;
+template<typename T>
+using priority_queue_rev = priority_queue<T, vector<T>, greater<T>>; //小さいものから取り出す
 
 int main(){
 
     cin >> ;
 
-    cout <<  << endl;
+    cout << ans << endl;
 //  printf("%.10f\n",ans);
 
     return 0;
